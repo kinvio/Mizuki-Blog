915bb53a7fe00c7ed0380f06590f1ce2524e9975
Kinvio <3437579970@qq.com>
2026-09-12 23:51:45 +0800

    update

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -127,12 +127,16 @@ export const siteConfig: SiteConfig = {
 				"/assets/desktop-banner/第七期(冰冻鱼粽)预览版.png",
 				"/assets/desktop-banner/gpt1.png",
 				"/assets/desktop-banner/gpt4.png",
+				"/assets/mobile-banner/第十三期莉莉预览版3.冰冻鱼粽.png",
+				"/assets/mobile-banner/冰冻鱼粽子14期.png",
 			], // 桌面横幅图片
 			mobile: [
-				"/assets/mobile-banner/ll.png",
-				"/assets/mobile-banner/第七期(冰冻鱼粽)预览版.png",
-				"/assets/mobile-banner/gpt1.png",
-				"/assets/mobile-banner/gpt4.png",
+				"/assets/mobile-banner/QQ1.png",
+				"/assets/mobile-banner/QQ2.png",
+				"/assets/mobile-banner/QQ3.png",
+				"/assets/mobile-banner/QQ4.png",
+				"/assets/mobile-banner/QQ7.png",
+				
 			], // 移动横幅图片
 		}, // 使用本地横幅图片
 
